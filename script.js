58887b4470268837c8aa9910e618f325ffa4c7c2
acl13 <128934431+acl13@users.noreply.github.com>
2024-02-16 15:58:08 -0500

    add placeholder Book objects

--- script.js
+++ script.js
@@ -10,12 +10,20 @@ function Book(title, author, pages, read) {
     }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
-console.log(theHobbit.info());
+let book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
+let book2 = new Book('Of Mice and Men', 'John Steinbeck', 107, 'not read yet');
+let book3 = new Book('Animal Farm', 'George Orwell', 112, 'read');
+let book4 = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 189, 'read');
+let book5 = new Book('A Clockwork Orange', 'Anthony Burgess', 192, 'not read yet');
+
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-addBookToLibrary(theHobbit);
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+addBookToLibrary(book4);
+addBookToLibrary(book5);
 console.log(myLibrary);
