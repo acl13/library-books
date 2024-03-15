d6647dac83fe87a0057b4c3bf877c30b95d3ae9b
acl13 <128934431+acl13@users.noreply.github.com>
2024-03-15 13:49:29 -0400

    display form input info after submit button click

--- script.js
+++ script.js
@@ -1,50 +1,3 @@
-const myLibrary = [];
-
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read} ` 
-    }
-}
-
-
-function addBookToLibrary(title, author, pages, read) {
-    let book = new Book(title, author, pages, read);
-    myLibrary.push(book);
-}
-
-addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet');
-addBookToLibrary('Of Mice and Men', 'John Steinbeck', 107, 'not read yet');
-addBookToLibrary('Animal Farm', 'George Orwell', 112, 'read');
-addBookToLibrary('The Great Gatsby', 'F. Scott Fitzgerald', 189, 'read');
-addBookToLibrary('A Clockwork Orange', 'Anthony Burgess', 192, 'not read yet');
-
-
-let container = document.querySelector('#container');
-
-function displayBooks() {
-    for(book of myLibrary) {
-
-        let div = document.createElement('div');
-        let titleAndAuthor = document.createElement('p');
-        let pagesNum = document.createElement('p');
-        let readStatus = document.createElement('p');
-        titleAndAuthor.textContent = `${book.title} by ${book.author}`;
-        pagesNum.textContent = `${book.pages} pages`;
-        readStatus.textContent = `${book.read}`;
-      container.appendChild(div);
-      div.appendChild(titleAndAuthor);
-      div.appendChild(pagesNum);
-      div.appendChild(readStatus);
-    }
-
-}
-
-displayBooks();
-
 const openModal = document.getElementById('open-modal');
 const dialog = document.querySelector('dialog');
 const cancel = document.getElementById('cancel')
@@ -72,15 +25,24 @@ const pages = document.getElementById('pages');
 const read = document.getElementsByName('read');
 
 submit.addEventListener('click', () => {
+    
     console.log(title.value);
     console.log(author.value);
     console.log(pages.value);
     
     for (i = 0; i < read.length; i++) {
         if (read[i].checked) {
+            var radioValue = read[i].value;
             console.log(read[i].value);
         }
     }
+    
+
+    addBookToLibrary(title.value, author.value, pages.value, radioValue);
+    console.log(myLibrary);
+
+    displayBooks();
+    
 }
 )
 
@@ -88,4 +50,63 @@ submit.addEventListener('click', () => {
 submit.addEventListener('click', () => {
     dialog.close();
 })
+
+
+
+
+
+
+
+
+
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read} ` 
+    }
+}
+
+
+function addBookToLibrary(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet');
+addBookToLibrary('Of Mice and Men', 'John Steinbeck', 107, 'not read yet');
+addBookToLibrary('Animal Farm', 'George Orwell', 112, 'read');
+addBookToLibrary('The Great Gatsby', 'F. Scott Fitzgerald', 189, 'read');
+addBookToLibrary('A Clockwork Orange', 'Anthony Burgess', 192, 'not read yet');
+
+
+let container = document.querySelector('#container');
+
+
+
+
+function displayBooks() {
+    for(book of myLibrary) {
+        console.log(book);
+
+        let div = document.createElement('div');
+        let titleAndAuthor = document.createElement('p');
+        let pagesNum = document.createElement('p');
+        let readStatus = document.createElement('p');
+        titleAndAuthor.textContent = `${book.title} by ${book.author}`;
+        pagesNum.textContent = `${book.pages} pages`;
+        readStatus.textContent = `${book.read}`;
+      container.appendChild(div);
+      div.appendChild(titleAndAuthor);
+      div.appendChild(pagesNum);
+      div.appendChild(readStatus);
+    }
+
+}
+
+
 
