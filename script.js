9a3da68a3fcb9e17f7722ca48a6845d3dd46a5c1
acl13 <128934431+acl13@users.noreply.github.com>
2024-05-08 10:41:12 -0400

    refactor to use class syntax for Book constructor

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read} ` 
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
-    this.toggle = function() {
+  
+   
+    toggle () {
         if (this.read === 'yes') {
             this.read = 'no';
-            console.log(`read ${this.read}`)
             return this.read;
         } else if (this.read === 'no') {
             this.read = 'yes';
-            console.log(`read ${this.read}`)
             return this.read;
         }
     }
